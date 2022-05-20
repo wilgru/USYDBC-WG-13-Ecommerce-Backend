946187ab70c31a211be2dcc946874e688f4f1e22
wilgru <william.gruszka@gmail.com>
2022-05-20 22:45:20 +1000

    fixed bug - wasnt deleting records properly

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -59,7 +59,7 @@ router.put('/:id', async (req, res) => {
     // update a tag's name by its `id` value
     const tagToUpdate = await Tag.findByPk(req.params.id);
     if (!tagToUpdate) {
-      res.status(404).json("Error: No tag found!");
+      res.status(404).json("Error: Couldn't update because the tag was not found");
       return;
     }
 
@@ -76,25 +76,26 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagToDestroy = await Tag.findByPk(req.params.id);
-    const tagAssociations = await ProductTag.findAll({
+    const tagToDestroy = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagToDestroy){
+      res.status(404).json("Error: Couldn't delete because the tag was not found");
+      return;
+    }
+
+    // await tagToDestroy.destroy();
+    
+    await ProductTag.destroy({
       where: {
         tag_id: req.params.id
       }
     });
 
-    if (!tagToDestroy){
-      res.status(404).json("Error: No tag found!");
-      return;
-    }
-
-    await tagToDestroy.destroy();
     res.status(200).json(tagToDestroy);
-
-    if (tagAssociations.length) {
-      await tagAssociations.destroy();
-      res.status(200).json(tagAssociations);
-    }
 
   } catch (error) {
     res.status(500).json(error);
